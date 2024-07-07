73081d7657ca484a20d965d473f92cb7331f10ab
David Cramer <dcramer@gmail.com>
2024-07-07 13:59:15 -0700

    Add script to generate react component

---
+++ make-react.py
@@ -0,0 +1,72 @@
+import os
+import shutil
+import re
+import json
+
+
+def slugify(s):
+    s = s.lower().strip()
+    s = re.sub(r"[^\w\s-]", "", s)
+    s = re.sub(r"[\s_-]+", "-", s)
+    s = re.sub(r"^-+|-+$", "", s)
+    return s
+
+
+# https://github.com/lukes/ISO-3166-Countries-with-Regional-Codes
+with open("all-countries.json", "rb") as fp:
+    countries = json.load(fp)
+
+outdir = "dist"
+
+os.makedirs(outdir, 511, True)
+
+folders = [x for x in os.listdir("all") if len(x) == 2]
+
+import_statements = []
+case_statements = []
+for folder_name in folders:
+    svg = os.path.join("all", folder_name, "vector.svg")
+
+    # find country by alpha
+    for country in countries:
+        if country["alpha-2"].lower() == folder_name.lower():
+            break
+    else:
+        raise Exception("Cant find country: " + folder_name)
+
+    slug = slugify(country["name"])
+    shutil.copyfile(svg, os.path.join(outdir, slug + ".svg"))
+
+    norm_name = re.sub(r"[\s\(\),']", "", country["name"])
+    import_statements.append(
+        'import %sMap from "@peated/web/assets/countries/%s.svg";' % (norm_name, slug)
+    )
+    case_statements.append(
+        """    case "%s":
+       return <%sMap {...props} />;"""
+        % (slug, norm_name)
+    )
+
+print(
+    """
+import { type ComponentPropsWithoutRef } from "react";
+      
+%(import_statements)s
+
+export default function CountryMapIcon({
+  slug,
+  ...props
+}: ComponentPropsWithoutRef<"svg"> & { slug: string }) {
+  switch (slug) {
+%(case_statements)s
+    default:
+      return null;
+  }
+}
+
+"""
+    % {
+        "import_statements": "\n".join(import_statements),
+        "case_statements": "\n".join(case_statements),
+    }
+)
